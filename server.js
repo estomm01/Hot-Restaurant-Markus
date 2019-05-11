efd99989b07bff955385b6fd04e430ac48b558b4
Markus Maasikas <markus@hotmail.com>
2019-05-11 11:03:27 -0400

    updated server.js

--- server.js
+++ server.js
@@ -10,6 +10,49 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App listening on PORT " + PORT);
+});
+
+
+var customers = [];
+var waitlist = [];
+
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'reserve.html'));
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'home.html'));
+});
+
+app.get('/api/tables', function (req, res) {
+  for (var i = 0; i < 4; i++) {
+    res.json(customers[i]);
+  }
+  return res.json(customers);
+});
+
+app.get('/api/waitlist', function (req, res) {
+  for (var i = 5; i < customers.length; i++) {
+    res.json(customers[i]);
+  }
+  return res.json(waitlist);
+});
+
+app.post('/api/clear', function (req, res) {
+  customers = [];
+  waitlist = [];
+});
+
+app.post('/api/new', function(req, res) {
+	console.log('Works');
+	var newCustomer = req.body;
+	if (customers.length >= 5) {
+		waitlist.push(newCustomer);
+	} else {
+		customers.push(newCustomer);
+	}
+	res.json(newCustomer);
 });
