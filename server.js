880476298c696cb369539565b07be8ddb986ff7e
DavidmB <david.m.bedford1@gmail.com>
2019-05-11 10:41:41 -0400

    app listening on port 3000

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var path = require("path");
-var bodyParser = require(1"body-parser");
+var bodyParser = require("body-parser");
 // Sets up the Express App
 // =============================================================
 var app = express();
@@ -9,3 +9,7 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
