7851313a00d52cdacff986b4e319b273efff023f
rjshoemaker55 <rjshoemaker55@gmail.com>
2019-05-11 12:32:42 -0400

    fixed button js error

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require ('express');
 var path = require ('path');
 var bodyParser = require ('body-parser');
+
 // Sets up the Express App
 
 var app = express ();
 var PORT = process.env.PORT || 3000;
+
+var $ = require ('jquery');
 
 // Sets up the Express app to handle data parsing
 app.use (express.urlencoded ({extended: true}));
@@ -54,14 +57,6 @@ app.post ('/api/new', function (req, res) {
   }
   res.json (newCustomer);
 });
-
-$(document).ready(function () {
-  $ ('#reservation').on ('click', function () {
-    res.sendFile (path.join (__dirname, 'reserve.html'));
-  });
-});
-
-
 
 app.listen (PORT, function () {
   console.log ('App listening on PORT ' + PORT);
